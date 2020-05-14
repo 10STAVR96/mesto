e0e9ad603b06a559d076d3a12cabecfe16af9571
Vitaliy Maksimov <vetal199624@mail.ru>
2020-05-14 03:21:42 +0300

    js processing v3

--- js/script.js
+++ js/script.js
@@ -59,9 +59,13 @@ const prepend = 'prepend';
 
 const clearErrors = (popupName, formObject) => {         /*очищение ошибок*/
     const inputElements = Array.from(popupName.querySelectorAll(formObject.inputSelector));
-    inputElements.forEach((input) => {
-        hideInputError(popupName, input, formObject);
-    });
+    const inputButton = popupName.querySelector(formObject.submitButtonSelector);
+    if (inputElements.length > 0) {
+        toggleButtonState(inputElements, inputButton, formObject);       /*данная функция здесь нужна для деактивации кнопки submit формы добавления карточки. Без нее при добавлении карточки и открытии формы повторно, кнопка submit активна не смотря на пустые поля*/
+        inputElements.forEach((input) => {
+            hideInputError(popupName, input, formObject);
+        });
+    }
 };
 
 /* ================================================
