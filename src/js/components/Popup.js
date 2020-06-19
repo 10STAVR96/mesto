c0562551e8eec46288e61b732b99527ed06f22a9
Vitaliy Maksimov <vetal199624@mail.ru>
2020-06-19 20:27:10 +0300

    OOP and webpack v5 fixed

--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -1,6 +1,16 @@
 export default class Popup {   /*открытия/закрытия всех popup блоков*/
     constructor(popupSelector) {
         this._popupSelector = popupSelector;
+        this._handleEscClose = (evt) => {
+            if (evt.key === 'Escape') {
+                this.close();
+            }
+        };
+        this._handleOverlayClose = (evt) => {
+            if (evt.target.classList.contains('popup_opened')) {
+                this.close();
+            }
+        };
     }
     open() {
         this._popupSelector.classList.add('popup_opened');
@@ -8,20 +18,13 @@ export default class Popup {   /*открытия/закрытия всех popu
     }
     close() {
         this._popupSelector.classList.remove('popup_opened');
+        document.removeEventListener('keydown', this._handleEscClose);
+        document.removeEventListener('click', this._handleOverlayClose);
     }
-    _handleEscClose(evt) {
-        if (evt.key === 'Escape') {
-            this.close();
-        }
-    }
-    _handleOverlayClose(evt) {
-        if (evt.target.classList.contains('popup_opened')) {
-            this.close();
-        }
-    }
+    
     _setEventListeners() {
         this._popupSelector.querySelector('.popup__close').addEventListener('click', () => this.close());
-        document.addEventListener('keydown', evt => this._handleEscClose(evt));
-        document.addEventListener('click', evt => this._handleOverlayClose(evt));
+        document.addEventListener('keydown', this._handleEscClose);
+        document.addEventListener('click', this._handleOverlayClose);
     }
 }
--- src/js/components/PopupWithForm.js
+++ src/js/components/PopupWithForm.js
@@ -4,13 +4,17 @@ export default class PopupWithForm extends Popup {
     constructor({submitFormHandler}, popupSelector) {
         super(popupSelector);
         this._submitFormHandler = submitFormHandler;
+        this._inputList = this._popupSelector.querySelectorAll('.popup__input');
+        this._submit = evt => {
+            evt.preventDefault();
+            this._submitFormHandler(this._getInputValues());
+            this.close();
+        };
     }
     close() {
         super.close();
-        if (this._popupSelector.id === 'form-card') {    /*можно было сделать также как в _getInputValues, но не уверен что нам нужно чистить инпуты профиля после сохранения, поэтому сделал с условной конструкцией*/
-            this._popupSelector.querySelector('#card-name').value = '';
-            this._popupSelector.querySelector('#card-url').value = '';
-        }
+        this._popupSelector.querySelector('.popup__container').reset();
+        this._popupSelector.querySelector('.popup__container').removeEventListener('submit', this._submit);
     }
     _getInputValues() {
         this._inputList = this._popupSelector.querySelectorAll('.popup__input');
@@ -22,10 +26,6 @@ export default class PopupWithForm extends Popup {
     }
     _setEventListeners() {
         super._setEventListeners();
-        this._popupSelector.querySelector('.popup__container').addEventListener('submit', (evt) => {
-            evt.preventDefault();
-            this._submitFormHandler(this._getInputValues());
-            this.close();
-        }, {once: true});
+        this._popupSelector.querySelector('.popup__container').addEventListener('submit', this._submit);
     }
 }
